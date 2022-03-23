b077e6a97363b4440632259e1cb7ab763ec4bd94
hanandadras <hanan.aabid@gmail.com>
2022-03-23 13:15:26 -0700

    created navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <a className="navbar-brand" href="About me">RESUME</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    {/* <FontAwesome icon={faUser} style={{ color: "#fff"}}/> */}
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="Projects">Home <span className="sr-only"></span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#top">about me</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#top">work</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#top">portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#top">contact</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
+
